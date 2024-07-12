9f7ebbba4985ad61d798817a6daa53021acdd590
dmkkj <40270618+dmkkj@users.noreply.github.com>
2024-07-12 17:48:08 +0900

    20240712

--- 2_Java/08_Polymorphism/src/com/kh/practice/model/Member.java
+++ 2_Java/08_Polymorphism/src/com/kh/practice/model/Member.java
@@ -1,7 +1,9 @@
 package com.kh.practice.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
+/*
 public class Member {
 	
 	private String name;
@@ -10,7 +12,6 @@ public class Member {
 	private Book[] bookList = new Book[2];
 	
 	public Member() {
-		
 	}
 
 	public Member(String name, int age, int coupon, Book[] bookList) {
@@ -58,4 +59,62 @@ public class Member {
 				+ Arrays.toString(bookList) + "]";
 	}
 
+}
+*/
+
+//ArrayList 사용
+public class Member {
+
+	private String name;
+	private int age;
+	private int coupon;
+	private ArrayList<Book> bookList = new ArrayList<>();
+	
+	public Member() {
+	}
+
+	public Member(String name, int age, int coupon, ArrayList<Book> bookList) {
+		this.name = name;
+		this.age = age;
+		this.coupon = coupon;
+		this.bookList = bookList;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getCoupon() {
+		return coupon;
+	}
+
+	public void setCoupon(int coupon) {
+		this.coupon = coupon;
+	}
+
+	public ArrayList<Book> getBookList() {
+		return bookList;
+	}
+
+	public void setBookList(ArrayList<Book> bookList) {
+		this.bookList = bookList;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [name=" + name + ", age=" + age + ", coupon=" + coupon + ", bookList=" + bookList + "]";
+	}
+	
 }
